df5bcd96ec6910fab1e7f5ffa9e6ae9882e6e2dd
Anthony Green <Anthonydgreen90@gmail.com>
2018-07-01 17:04:18 -0700

    working with local database

--- Lab17_ToDoApi/Startup.cs
+++ Lab17_ToDoApi/Startup.cs
@@ -10,22 +10,28 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Lab17_ToDoApi.Models;
+using TodoApi.Models;
 
 namespace Lab17_ToDoApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,13 +41,13 @@ namespace Lab17_ToDoApi
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
+
+            app.UseMvc();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
-            app.UseMvc();
         }
     }
 }
